843ddea0463de27683c26a648301f98a6535d787
Ruy Almeida <ruy.fsalmeida@gmail.com>
2018-01-17 16:31:38 -0200

    binary conversor creation

---
+++ projects/binary_conversor/js/Converter.js
@@ -0,0 +1,25 @@
+class Converter {
+
+	convertDecimalToBinary(input){
+		var remainders = new Array();
+
+		do {
+			var remainder = input % 2;
+			remainders.push(remainder);
+
+			input = input / 2;
+		} while (input >= 1);
+
+		return this.convertArrayToReverseString(remainders);
+	}
+
+	convertArrayToReverseString(input){
+		var reversedString = "";
+
+		while(input.length > 0)
+			reversedString += input.pop();
+
+		return reversedString;
+	}
+
+}
